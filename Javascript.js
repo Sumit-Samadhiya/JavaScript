877272e2f516f0b66a905b57e2512c1ff20995b7
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-07-27 15:26:05 +0530

    js

--- Javascript.js
+++ Javascript.js
@@ -4,5 +4,50 @@ JavaScript is a high-level, dynamic, and interpreted programming language that i
 
 */
 
+/* 
+ What are the data types in JavaScript?
+ JavaScript supports multiple data types, which are broadly categorized into primitive and non-primitive types. The primitive data types include:
+
+1. Number
+The number type in JavaScript contains both integer and floating-point numbers. Besides regular numbers, there are also some special numbers: Infinity, -Infinity, and NaN. Infinity represents the mathematical infinity, NaN denotes a computational error.
+
+let num = 2; // Integer
+let num2 = 1.3; // Floating point number
+let num3 = Infinity; // Infinity
+let num4 = 'something here too'/2; // NaN
+
+2. String
+A String in JavaScript is a series of characters that are surrounded by quotes. There are three types of quotes in JavaScript: single quotes, double quotes, and backticks. Backticks provide extra functionality, allowing you to embed variables inside them.
+
+let str = "Hello There";
+let str2 = 'Single quotes works fine';
+let phrase = can embed ${someVariable};
+
+
+3. Boolean
+The boolean type has only two values: true and false. This data type is used to store yes/no values: true means “yes, correct”, and false means “no, incorrect”.
+
+let isCoding = true; // yes
+let isOld = false;      // no
+
+4. Null
+The special null value does not belong to any of the default data types. It forms a separate type of its own, which contains only the null value. null basically defines a special value that represents ‘nothing’, ’empty’, or ‘value unknown’.
+
+let age = null;
+
+5. Undefined
+Undefined makes its own type. The meaning of undefined is ‘value is not assigned’.
+
+let x;
+console.log(x);  // undefined
+
+6. Symbol
+Symbols are new primitive built-in object types introduced as part of ES6.
+
+The non-primitive types include Object, Array, and Function.
+
+Note that BigInt is also a primitive type in JavaScript, which is used to represent big integers more accurately.
+
+ */
 
 
