b9ce7b008802cad67da3b2afd763b3cbb5843477
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-07-27 15:22:55 +0530

    js

--- Javascript.js
+++ Javascript.js
@@ -1,6 +1,8 @@
-// What is JavaScript? 
-// JavaScript is a high-level, dynamic, and interpreted programming language that is primarily used for client-side scripting.
+/*
+What is JavaScript? 
+JavaScript is a high-level, dynamic, and interpreted programming language that is primarily used for client-side scripting.
 
+*/
 
 
 
